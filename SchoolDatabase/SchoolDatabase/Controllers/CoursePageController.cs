98760253ca617d9ddd78b9ebd33e8d307f9faaf9
Joyce Lin <ohanalin@gmail.com>
2024-11-15 16:27:13 -0500

    push

---
+++ SchoolDatabase/SchoolDatabase/Controllers/CoursePageController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using SchoolDatabase.Models;
+
+
+namespace SchoolDatabase.Controllers
+{
+    public class CoursePageController : Controller
+    {
+
+        private readonly CourseAPIController _api;
+
+        public CoursePageController(CourseAPIController api)
+        {
+            _api = api;
+        }
+        public IActionResult List()
+        {
+            List<Course> Courses = _api.ListCourses();
+            return View(Courses);
+        }
+        public IActionResult Show(int id)
+        {
+            Course SelectedCourse = _api.FindCourse(id);
+
+            return View(SelectedCourse);
+        }
+    }
+}
---
+++ SchoolDatabase/SchoolDatabase/Controllers/StudentPageController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolDatabase.Models;
+
+namespace SchoolDatabase.Controllers
+{
+    public class StudentPageController : Controller
+    {
+
+        private readonly StudentAPIController _api;
+
+        public StudentPageController(StudentAPIController api)
+        {
+            _api = api;
+        }
+        public IActionResult List()
+        {
+            List<Student> Students = _api.ListStudents();
+            return View(Students);
+        }
+        public IActionResult Show(int id)
+        {
+            Student SelectedStudent = _api.FindStudent(id);
+
+          
+
+            foreach (Student s in _api.ListStudents())
+            {
+                if (s.studentid == id)
+                    SelectedStudent= s;
+                else View("Not found");
+
+
+            }
+
+            return View(SelectedStudent);
+        }
+    }
+}
